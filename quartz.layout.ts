689c141e1c5e5706616111c9f0ea87d856fbe4d5
X-uman <achromaticsuman@gmail.com>
2024-06-03 18:15:20 +0545

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Facebook": "https://www.facebook.com/",
     },
   }),
 }
