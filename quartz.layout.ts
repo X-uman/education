fc47cbc3f8bcd2134cc8b027aae7f2428e8f8f56
X-uman <achromaticsuman@gmail.com>
2024-06-07 10:55:08 +0545

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,7 +31,6 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     Component.DesktopOnly(Component.Explorer()),
-    Component.Graph(),
     Component.Backlinks(),
     
   ],
