641497215609c1323d54b295b76460ca77b0751b
X-uman <achromaticsuman@gmail.com>
2024-06-07 13:05:59 +0545

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -15,13 +15,13 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Search(),
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
+    Component.Search(),
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.DesktopOnly(Component.TableOfContents()),
